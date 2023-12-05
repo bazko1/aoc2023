979f4ff156893af70868f3c4f3729e6c76567eca
Kot, Bazyli <bazylix.kot@intel.com>
2023-12-05 15:23:05 +0100

    implemented part one

--- day3/main.go
+++ day3/main.go
@@ -4,26 +4,47 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 func main() {
 
-	bytes, err := os.ReadFile("simple.txt")
+	bytes, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fileContent := string(bytes)
 	lines := strings.Split(fileContent, "\n")
-	digitRe := regexp.MustCompile("[0-9]+")
-	symbolRe := regexp.MustCompile("[^.]+")
-	var symbols [][]int
-	var digits [][]int
-	for _, line := range lines {
-		lineDigits := digitRe.FindAllStringIndex(line, -1)
-		lineSymbols := symbolRe.FindAllStringIndex(line, -1)
-		fmt.Println(lineDigits)
-		fmt.Println("symb:=", lineSymbols)
+	lines = lines[:len(lines)-1]
+	var sum int
+	digitStack := ""
+	isAdjacent := false
+	for row, line := range lines {
+		for col, ch := range line {
+			if unicode.IsDigit(ch) {
+				digitStack += string(ch)
+				for x := -1; x < 2; x++ {
+					for y := -1; y < 2; y++ {
+						if X, Y := col+x, row+y; X >= 0 && X < len(line) &&
+							Y >= 0 && Y < len(lines) &&
+							!unicode.IsDigit([]rune(lines[Y])[X]) &&
+							[]rune(lines[Y])[X] != '.' {
+							isAdjacent = true
+						}
+					}
+				}
+			} else {
+				if digitStack != "" && isAdjacent {
+					val, _ := strconv.Atoi(digitStack)
+					sum += val
+					// fmt.Println("Adding", val, col, row)
+				}
+				digitStack = ""
+				isAdjacent = false
+			}
+		}
 	}
+	fmt.Println(sum)
 }
